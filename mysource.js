3e641588f09496ec403b6b45c5ec40b4d93665d6
Gaurav7397 <58032126+Gaurav7397@users.noreply.github.com>
2022-02-08 02:30:34 +0530

    Update mysource.js

--- mysource.js
+++ mysource.js
@@ -2,6 +2,7 @@ var element = document.getElementById("team");
 element.addEventListener("click", myFunction);
 
 function myFunction() {
+  alert('In the Loop');
   document.getElementById('test').innerHTML="In the ssd";
 }
 
