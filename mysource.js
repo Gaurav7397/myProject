4a372512640dce5b57e45d0f44019b35912cd292
Gaurav7397 <58032126+Gaurav7397@users.noreply.github.com>
2022-02-08 02:21:23 +0530

    Update mysource.js

--- mysource.js
+++ mysource.js
@@ -2,6 +2,6 @@ const element = document.getElementById("team");
 element.addEventListener("click", myFunction);
 
 function myFunction() {
-  document.getElementById('test').innerHTML="In the Javascript";
+  document.getElementById('test').innerHTML="In the ssd";
 }
 
