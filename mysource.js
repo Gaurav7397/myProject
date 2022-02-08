644fb5034135b65226e18911f8f99180c1779d10
Gaurav7397 <58032126+Gaurav7397@users.noreply.github.com>
2022-02-09 01:04:35 +0530

    Update mysource.js

--- mysource.js
+++ mysource.js
@@ -1,6 +1,7 @@
 var myButton = document.getElementById("my-dark-button");
 myButton.addEventListener("click",myfunction);
 function myfunction(){
+  alert("Change");
   document.body.style.backgroundColor="black";
 }
 
