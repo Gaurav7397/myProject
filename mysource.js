c4c60d4c5358a5f92edd37027878c3bef37b9c6f
Gaurav7397 <58032126+Gaurav7397@users.noreply.github.com>
2022-02-08 23:30:39 +0530

    Update mysource.js

--- mysource.js
+++ mysource.js
@@ -1,4 +1,4 @@
-let myButton = document.getElementById("my-dark-button");
+var myButton = document.getElementById("my-dark-button");
 myButton.addEventListener("click",myfunction);
 function myfunction(){
   alert("In the script");
